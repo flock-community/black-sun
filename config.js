7668735867e3ba0a86bae832f899e2994ccf1b3f
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2024-04-18 13:24:26 +0200

    update config with additional build paths
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 export const TOKENS_ROOT_DIR = path.resolve(__dirname, "./tokens/");
 export const TOKENS_FILE_PATH = path.resolve(TOKENS_ROOT_DIR, "**/*.json");
 export const BUILD_PATH = path.join(__dirname, "build/");
-export const BUILD_PATH_WEB = path.join(BUILD_PATH, "js/");
+export const BUILD_PATH_INFIMA = path.join(BUILD_PATH, "infima/");
+export const BUILD_PATH_JS = path.join(BUILD_PATH, "js/");
