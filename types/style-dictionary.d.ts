33d9cf8bd19d2ec10bc4beca7598617ed75f3d9c
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2025-01-13 09:01:48 +0100

    chore: add global types for transformers
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- types/style-dictionary.d.ts
+++ types/style-dictionary.d.ts
@@ -1,5 +1,13 @@
-import type { FileHeader as _FileHeader } from "style-dictionary/types";
+import type {
+	FileHeader as _FileHeader,
+	NameTransform as _NameTransform,
+	Transform as _Transform,
+} from "style-dictionary/types";
 
 declare global {
-	export { _FileHeader as FileHeader };
+	export {
+		_FileHeader as FileHeader,
+		_NameTransform as NameTransform,
+		_Transform as Transform,
+	};
 }
